422dd8fedd6f18a9dd77280fdb7407e3521b3c1d
Nathan Solis <66754842+coldenate@users.noreply.github.com>
2023-04-08 21:35:51 -0500

    fix: broken http promise

--- themes/build.js
+++ themes/build.js
@@ -1,4 +1,3 @@
-const request = require("request");
 const less = require("less");
 const fs = require("fs");
 const { variants, labels } = require("@catppuccin/palette");
@@ -7,98 +6,96 @@ const themes = ["latte", "frappe", "macchiato", "mocha"];
 
 const url = "https://raw.githubusercontent.com/catppuccin/palette/main/less/";
 
+let masterTheme = fs.readFileSync("src/theme.less", "utf8");
+
 fs.mkdir("built", { recursive: true }, (err) => {
 	if (err && err.code !== "EEXIST") {
 		throw err;
 	}
 });
 
-console.log("Reading theme.less file");
-fs.readFile("src/theme.less", "utf8", (err, data) => {
-	if (err) {
-		console.error(err);
-		return;
+function formTheme(theme) {
+	// replace appearance with .light or .dark
+	let themeData = masterTheme.replace(
+		/"appearance"/g,
+		theme === "latte" ? ".light" : ".dark"
+	);
+
+	// prepend the color palette to the less file
+	let palette = "";
+
+	for (const colorName in variants[theme]) {
+		let color = variants[theme][colorName];
+		palette += `@${colorName}-raw: ${color.raw};\n`;
+		palette += `@${colorName}-hsl: ${color.hsl};\n`;
+		palette += `@${colorName}-rgb: ${color.rgb};\n`;
+		palette += `@${colorName}: ${color.hex};\n`;
 	}
-	// iterate through themes
-	themes.forEach((theme) => {
-		// replace appearance with .light or .dark
-		const themeData = data.replace(
-			/"appearance"/g,
-			theme === "latte" ? ".light" : ".dark"
-		);
 
-		// fetch the less file from github
-		console.log("Fetching less file from GitHub");
-		request(url + theme + ".less", (err, res, body) => {
+	themeData = palette + themeData;
+
+	let output = "";
+
+	// compile the less file
+	return new Promise((resolve, reject) => {
+		// compile the less file
+		less.render(themeData, (err, output) => {
 			if (err) {
-				console.error(err);
-				return;
+				reject(err);
+			} else {
+				resolve(output.css);
 			}
+		});
+	});
+}
 
-			for (const colorName in variants[theme]) {
-				let color = variants[theme][colorName];
-				body += `@${colorName}-raw: ${color.raw};\n`;
-				body += `@${colorName}-hsl: ${color.hsl};\n`;
-				body += `@${colorName}-rgb: ${color.rgb};\n`;
-			}
+function formManifest(theme) {
+	let manifest = fs
+		.readFileSync("src/manifest.template.json", "utf8")
+		.replace(/<theme name>/g, theme)
+		.replace(/<appearance>/g, theme === "latte" ? "light" : "dark")
+		.replace(/<theme name cap>/g, theme[0].toUpperCase() + theme.slice(1))
+		.replace(/<authors>/g, "justTOBBI, coldenate");
 
-			console.log("Writing less file to theme.less");
-			let newFile = body + themeData;
-			console.log("Compiling theme.less file");
-			less.render(newFile, (err, output) => {
+	return manifest;
+}
+
+function getLogo(theme) {
+	return theme === "latte" ? "src/wlogo.png" : "src/logo.png";
+}
+
+//
+
+// iterate through themes
+// run the fuinction to form the theme
+themes.forEach(async (theme) => {
+	try {
+		let themeFile = await formTheme(theme);
+
+		let manifest = formManifest(theme);
+		let logo = getLogo(theme);
+
+		console.log("Creating theme folder");
+
+		const dir = "built/" + theme;
+		if (!fs.existsSync(dir)) {
+			fs.mkdirSync(dir);
+		}
+
+		console.log("Writing CSS file to theme folder");
+		fs.writeFile("built/" + theme + "/theme.css", themeFile, (err) => {
+			console.log("Copying manifest.json to theme folder");
+			fs.writeFileSync("built/" + theme + "/manifest.json", manifest);
+
+			console.log("Copying logo.png to theme folder");
+			fs.copyFile(logo, "built/" + theme + "/logo.png", (err) => {
 				if (err) {
 					console.error(err);
 					return;
 				}
-				console.log("Creating theme folder");
-
-				const dir = "built/" + theme;
-				if (!fs.existsSync(dir)) {
-					fs.mkdirSync(dir);
-				}
-
-				console.log("Writing CSS file to theme folder");
-				fs.writeFile(
-					"built/" + theme + "/theme.css",
-					output.css,
-					(err) => {
-						if (err) {
-							console.error(err);
-							return;
-						}
-						console.log("Copying manifest.json to theme folder");
-						let manifest = fs
-							.readFileSync("src/manifest.template.json", "utf8")
-							.replace(/<theme name>/g, theme)
-							.replace(
-								/<appearance>/g,
-								theme === "latte" ? "light" : "dark"
-							)
-							.replace(
-								/<theme name cap>/g,
-								theme[0].toUpperCase() + theme.slice(1)
-							)
-							.replace(/<authors>/g, "justTOBBI, coldenate");
-						fs.writeFileSync(
-							"built/" + theme + "/manifest.json",
-							manifest
-						);
-						console.log("Copying logo.png to theme folder");
-						fs.copyFile(
-							theme === "latte"
-								? "src/wlogo.png"
-								: "src/logo.png",
-							"built/" + theme + "/logo.png",
-							(err) => {
-								if (err) {
-									console.error(err);
-									return;
-								}
-							}
-						);
-					}
-				);
 			});
 		});
-	});
+	} catch (err) {
+		console.error(err);
+	}
 });
